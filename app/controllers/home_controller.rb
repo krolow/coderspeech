10c1c04fb5d042d7ba458a9312701aa76f5eb0c6
Vinícius Krolow <krolow@gmail.com>
2014-07-28 02:27:08 -0300

    Changing to show 8 elements for recent and view videos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @featured = Video.recent.featured.first
-    @latest_videos = Video.published.recent.limit(6)
-    @most_viewed_videos = Video.published.most_viewed.limit(6)
+    @latest_videos = Video.published.recent.limit(8)
+    @most_viewed_videos = Video.published.most_viewed.limit(8)
   end
 end
