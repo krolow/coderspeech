52131198ec5bcd3baff96218ad41ea1bc861150b
Vinícius Krolow <krolow@gmail.com>
2014-07-28 04:26:08 -0300

    Random feature order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @featured = Video.recent.featured.first
+    @featured = Video.published.featured.order("RANDOM()").first
     @latest_videos = Video.published.recent.limit(8)
     @most_viewed_videos = Video.published.most_viewed.limit(8)
   end
